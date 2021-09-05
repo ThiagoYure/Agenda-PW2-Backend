44653dbafa306128664ca54dd7e0691c75ab0bc1
ThiagoYure <thigoyure@gmail.com>
2021-09-05 12:50:26 -0300

    Configurações Heroku

--- models/user.js
+++ models/user.js
@@ -1,8 +1,9 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const Contato = require('./contato');
 
-var sequelize = new Sequelize('email-check', 'postgres', '1234', {
-  host: 'localhost',
+var sequelize = new Sequelize('d1630bvmdrvpqe', 'zvjxyhswcbgprc', '8a03c4fa27752af0574fcb679b596b49d8855fb055dbadc22a0b9d3d4941a013', {
+  host: 'ec2-54-145-188-92.compute-1.amazonaws.com',
+  port: 5432,
   dialect: 'postgres'
 });
 
