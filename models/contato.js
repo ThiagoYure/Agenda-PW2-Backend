ab2924466d2eb175502f2af06104efdb82381bd0
ThiagoYure <thigoyure@gmail.com>
2021-09-05 13:52:51 -0300

    Postgres configuração url

--- models/contato.js
+++ models/contato.js
@@ -1,12 +1,7 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const User = require('./user');
 
-var sequelize = new Sequelize('d1630bvmdrvpqe', 'zvjxyhswcbgprc', '8a03c4fa27752af0574fcb679b596b49d8855fb055dbadc22a0b9d3d4941a013', {
-  host: 'ec2-54-145-188-92.compute-1.amazonaws.com',
-  port: 5432,
-  dialect: 'postgres',
-  ssl: true
-});
+var sequelize = new Sequelize(process.env.DATABASE_URL);
 
 const Contato = sequelize.define('contato', {
   id: {
